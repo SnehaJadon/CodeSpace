87028020a7a218d6693f29bd5c84a653bffc613e
Sneha Jadon <93697009+SnehaJadon@users.noreply.github.com>
2025-02-10 20:06:24 +0530

    create EditorPanel and OutputPanel

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import EditorPanel from "./_components/EditorPanel";
 import Header from "./_components/Header"
+import OutputPanel from "./_components/OutputPanel";
 
 export default function Home() {
   return (
@@ -8,8 +10,8 @@ export default function Home() {
         <Header/>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-          {/* <EditorPanel /> */}
-          {/* <OutputPanel /> */}
+          <EditorPanel />
+          <OutputPanel />
         </div>
       </div>
     </div>
