890b5ce7a46184ccc1d381b55a1e208eee6b1ad9
Sneha Jadon <93697009+SnehaJadon@users.noreply.github.com>
2025-02-11 08:07:20 +0530

    create price component, create and add lemon squeezy webhook and handle http action for convex and final commit

---
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -0,0 +1,19 @@
+import { Zap } from "lucide-react";
+import Link from "next/link";
+
+export default function UpgradeButton() {
+  const CHEKOUT_URL =
+    "https://codespace.lemonsqueezy.com/buy/8588106c-14fa-48da-a0f5-e7657b2ab1f7";
+
+  return (
+    <Link
+      href={CHEKOUT_URL}
+      className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white 
+        bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg 
+        hover:from-blue-600 hover:to-blue-700 transition-all"
+    >
+      <Zap className="w-5 h-5" />
+      Upgrade to Pro
+    </Link>
+  );
+}
